fd1d584a155312e182bee725be3ac845eae8e2eb
abdalahshaban <abdalahshaban@yahoo.com>
2020-03-19 15:32:43 +0200

    until custom Drawer

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:social_ui/data/data.dart';
+import 'package:social_ui/widgets/custom_drawer.dart';
+import 'package:social_ui/widgets/following_users.dart';
+import 'package:social_ui/widgets/posts_carousel.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _pageController = PageController(
+      initialPage: 0,
+      viewportFraction: 0.8,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text(
+          'FRENZY',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
+          ),
+        ),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 13.0,
+          ),
+          unselectedLabelColor: Colors.grey,
+          indicatorWeight: 3.0,
+          indicatorColor: Colors.blueGrey,
+          indicatorSize: TabBarIndicatorSize.tab,
+          tabs: <Widget>[
+            Tab(
+              text: 'Trending',
+
+              icon: Icon(
+                Icons.trending_up,
+              ),
+            ),
+            Tab(
+              text: 'Latest',
+              icon: Icon(Icons.fiber_new),
+            ),
+          ],
+        ),
+      ),
+      drawer: CustomDrawer(),
+      drawerScrimColor: Colors.transparent,
+      body: ListView(
+        children: <Widget>[
+          FollowingUsers(),
+          PostsCarousel(
+            pageController: _pageController,
+            title: 'Posts',
+            posts: posts,
+          ),
+        ],
+      ),
+    );
+  }
+}
