da56db92ac4e54fca4ce768f5e5fa7ebdbebc48d
abdalahshaban <abdalahshaban@yahoo.com>
2020-03-19 16:15:30 +0200

    drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:social_ui/data/data.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Image(
+                height: 250.0,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  currentUser.backgroundImageUrl,
+                ),
+              ),
+              Positioned(
+                bottom: 20.0,
+                left: 20.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 3.0,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      child: ClipOval(
+                        child: Image(
+                          image: AssetImage(
+                            currentUser.profileImageUrl,
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 6.0,
+                    ),
+                    Text(
+                      currentUser.name,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
